672610353204286c1e2983896455cbcb2c63fc68
Vlad <vlad_domnich@mail.ru>
2017-01-22 11:45:43 +0200

    remade the class MyConstants

--- src/com/vlad/example/MyConstants.java
+++ src/com/vlad/example/MyConstants.java
@@ -3,7 +3,25 @@ package com.vlad.example;
 /**
  * Created by Влад on 09.11.2016.
  */
-public class MyСonstants {
+public class MyConstants {
 
     public static final String INPUT_NUMBER = "введите число";
+    public static final String VIEW_SCREEN_TITLE = "Калькулятор";
+    public static final String VIEW_LABEL_NUM_1 = "Число 1";
+    public static final String VIEW_LABEL_NUM_2 = "Число 2";
+    public static final String VIEW_LABEL_RESULT = "Результат";
+    public static final String VEIW_BUTTON_ADD = "Сложение";
+    public static final String VIEW_BUTTON_SUB = "Вычитание";
+    public static final String VIEW_BUTTON_MULT = "Умножение";
+    public static final String VIEW_BUTTON_DIV = "Деление";
+
+    public static final int BORDER_EMPTY = 15;
+    public static final int TEXT_FIELD_WIDTH = 10;
+    public static final int DISTANCE_BETWEEN_COMPONENTS = 7;
+    public static final int DISTANCE_BETWEEN_BUTTONS = 12;
+    public static final int DISTANCE_BETWEEN_LAYOUT_VERTICAL = 15;
+
+    public static final String VIEW_MASSEGE_DIALOG_NO_EMPTY_ALL_FIELD = "Поля для ввода не могут быть пустыми!";
+    public static final String VIEW_MASSEGE_DIALOG_NO_LITERA = "Вводимые данные должны быть числовые!";
+    public static final String VIEW_MASSEGE_DIALOG_EMPTY_ONE_FIELD = "Вы не ввели одно число";
 }
